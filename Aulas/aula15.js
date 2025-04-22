e25e9e47bac1f92b014ad9381e4cb1338eb3a46a
Neves <vitor.g.neves7@aluno.senai.br>
2025-04-22 07:18:03 -0300

    aulas

--- Aulas/aula15.js
+++ Aulas/aula15.js
@@ -1,31 +1,31 @@
 const prompt = require('prompt-sync')();
 
-// //criano nossa primira função
-// function cabecalho(texto){
-//     console.log('...............................')
-//     console.log(texto)
-//     console.log('...............................')
-// }
-// //criando outra função
-// function saudacao(){
-//     let nome = prompt('Digite seu nome: ')
-//     console.log(`${nome} tenha um bom dia `)
-// }
-// //chamando a função
-// cabecalho('SESI/SENAI')
-// //posso declarar umavariavel a passa-la como parametro para função
-// let escola= 'sesi 025'
-// cabecalho(escola)
-// saudacao()
+//criano nossa primira função
+function cabecalho(texto){
+    console.log('...............................')
+    console.log(texto)
+    console.log('...............................')
+}
+//criando outra função
+function saudacao(){
+    let nome = prompt('Digite seu nome: ')
+    console.log(`${nome} tenha um bom dia `)
+}
+//chamando a função
+cabecalho('SESI/SENAI')
+//posso declarar umavariavel a passa-la como parametro para função
+let escola= 'sesi 025'
+cabecalho(escola)
+saudacao()
 
-// //criei a função que resebera os valores como parametro a partir desses valores, realizando o calculo e mostralo
-// function soma(n1, n2){
-//     let resutado = n1 +n2
-//     console.log(resutado)
-// }
-// soma(5, 6)
-// soma(8, 9)
-// soma(8, 18)
+//criei a função que resebera os valores como parametro a partir desses valores, realizando o calculo e mostralo
+function soma(n1, n2){
+    let resutado = n1 +n2
+    console.log(resutado)
+}
+soma(5, 6)
+soma(8, 9)
+soma(8, 18)
 
 //Exercicios
 function Parlmpar(n){
