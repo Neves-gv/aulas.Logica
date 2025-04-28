ee12b88f0e06155266b68af53bc9be1c0c6101ab
Neves <vitor.g.neves7@aluno.senai.br>
2025-04-28 08:33:43 -0300

    excicios

--- exercicios/ex9/ex9_5.js
+++ exercicios/ex9/ex9_5.js
@@ -1,17 +1,15 @@
 // Crie uma função que gere uma senha aleatória, deve receber como parâmetro a
 // quantidade de caracteres que terá a senha, crie uma lista com todos os caracteres disponíveis
 // para a senha e com o random sorteie a qual de dígitos terá a senha
-function gerarSenha(tamanho) {
-    let caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{};:,.<>?";
+function gerarSenha(tamanhos) {
+    let caracteres = ["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{};:,.<>?"];
     let senha = "";
 
-    for (let i = 0; i < tamanho; i++) {
-        let indice = Math.floor(Math.random() * caracteres.length);
-        senha += caracteres[indice];
+    for (let i = 0; i < tamanhos; i++) {
+        let aleatoria = Math.floor(Math.random() * caracteres.length)
+        senha += caracteres[aleatoria]
     }
-
-    console.log(`Senha gerada: ${senha}`);
     return senha;
 }
-gerarSenha(10)
-gerarSenha(16)
+console.log('Senha gerada de 6 caracteris:', gerarSenha(6));
+console.log('Senha gerada de 6 caracteris:', gerarSenha(10));
