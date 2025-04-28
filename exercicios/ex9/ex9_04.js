af4473a49eee9e244a7b6898c2bfd0f6c1d3abdf
Neves <vitor.g.neves7@aluno.senai.br>
2025-04-28 08:18:13 -0300

    excicios

--- exercicios/ex9/ex9_04.js
+++ exercicios/ex9/ex9_04.js
@@ -2,11 +2,26 @@
 //operação (Ex. envio os parâmetros 2 e 5 e *, fazer a multiplicação), tratar as operações + - * / **
 const prompt = require('prompt-sync')()
 
-function Numeros(n1, n2){
-    if(n1 += n2){}
-    if(n1 -= n2){}
-    if(n1 /= n2){}
-    if(n1 *= n2){}
-    if(n1 **= n2){}
+function Numeros(n1, n2, operador){
+    if(operador === '+'){
+        console.log(`${n1} + ${n2} = ${n1 + n2}`)
 }
-console.log(Numeros(Number(prompt('digite um valor de inicio: '))), (prompt('digite um valor de inicio: ')))
+else if( operador === '-' ){
+    console.log(`${n1} - ${n2} = ${n1 - n2}`)
+}
+else if( operador === '*' ){
+    console.log(`${n1} * ${n2} = ${n1 * n2}`)}
+else if( operador === '/' ){
+        console.log(`${n1} / ${n2} = ${n1 / n2}`)
+ }else if( operador === '**' ){
+        console.log(`${n1} ** ${n2} = ${n1 ** n2}`)
+    }
+    else{console.log("Operador invalido")
+        return;
+    }
+}
+Numeros(2, 4, '+')
+Numeros(2, 4, '-')
+Numeros(2, 4, '*')
+Numeros(2, 4, '/')
+Numeros(2, 4, '**')
